f9d01fdc0fb612f67b43c673a82366c4dba7bfe3
vvv <ilyavvv@mail.ru>
2020-05-10 14:12:00 +0300

    fix errors

--- resources/views/label/edit.blade.php
+++ resources/views/label/edit.blade.php
@@ -3,6 +3,15 @@
 @section('content')
 <div class="container">
 @include('flash::message')
+@if ($errors->any())
+    <div class="alert alert-danger ">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 {{Form::model($label, ['url' => route('labels.update', $label), 'method' => 'PATCH'])}}
     {{ Form::label('name', 'Имя') }}
     {{ Form::text('name') }}
--- resources/views/task_status/edit.blade.php
+++ resources/views/task_status/edit.blade.php
@@ -3,6 +3,15 @@
 @section('content')
 <div class="container">
 @include('flash::message')
+@if ($errors->any())
+    <div class="alert alert-danger ">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 {{Form::model($taskStatus, ['url' => route('task_statuses.update', $taskStatus), 'method' => 'PATCH'])}}
     {{ Form::label('name', 'Имя') }}
     {{ Form::text('name') }}
